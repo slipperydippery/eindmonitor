d0333788c6527d7e13f32e8df283dd470690c7b3
Maarten de Jager <maartendejager@gmail.com>
2018-01-12 17:11:40 +0100

    basic layout homepage

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+ <!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Eindmonitor</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+		<div id="app">
+
+	        @yield('content')	
+	        
+        </div>
+
+         <!-- Scripts -->
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/bootstrap-slider.js"></script>
+         <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>   
+         <script>
+            window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!};
+        </script>
+         <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,52 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+@section('content')
+    <header>
+        <div class="banner">
+            <div class="container">
+                <img src="/img/logo_icons.svg" alt="" class="left">
+                <img src="/img/eindmonitor.svg" alt="" class="right eighty">
             </div>
         </div>
-    </body>
-</html>
+        <nav class="container">
+            <ul class="row">
+                <li>
+                    <a href="#">Aanleiding</a>
+                </li>
+                <li>
+                    <a href="#">Samenwerking</a>
+                </li>
+                <li>
+                    <a href="#">Fasering</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+    <div class="content">
+        <div class="page-highlight container">
+            <p>IJmond Bereikbaar is in 2015 gestart met als doel om de bereikbaarheid van de regio IJmond te verbeteren.Centraal staat structurele gedragsverandering in reisgedrag (woon/werkverkeer) van werknemers met werkbestemming in de IJmond. IJmond Bereikbaar wil hen stimuleren waar mogelijk de auto te laten staan en te kiezen voor de fiets of het OV om naar en van het werk te reizen. Deze eindmonitor laat u de de behaalde resultaten zien van de inspanningen.</p>
+            <p>Kies hieronder een thema en specifiek onderwerp en bekijk de resultaten.</p>
+        </div>
+        <div class="generator container row">
+            <div class="col-sm-4">
+                <a href="#" class="btn btn--fullwidth">werkgeversaanpak</a>
+            </div>
+            <div class="col-sm-2">
+                <img src="/img/arrows.svg" alt="" class="center hundred">
+            </div>
+            <div class="col-sm-4">
+                <a href="#" class="btn btn--fullwidth">resultaten</a>
+            </div>
+            <div class="col-sm-2">
+                <a href="#" class="btn">go</a>
+            </div>
+        </div>
+    </div>
+    <footer>
+        footer
+    </footer>
+@endsection
